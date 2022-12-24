8718141b6e1e1d910b0ae8c37850a5bf84c42e2e
Illia Kuznets <illiakuznets@gmail.com>
2022-12-25 00:46:52 +0200

    first commit

---
+++ src/main/java/com/example/invoiceautomation/InvoiceAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoiceautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoiceautomation/InvoiceAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoiceautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
